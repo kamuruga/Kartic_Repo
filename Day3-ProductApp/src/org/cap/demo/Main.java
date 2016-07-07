65f190bec5cec71aa2f71c31ef2d4d5c7cf8bf28
Training <rukmini.karuppaswamy@capgemini.com>
2016-07-07 10:05:47 +0530

    updataed main

--- Day3-ProductApp/src/org/cap/demo/Main.java
+++ Day3-ProductApp/src/org/cap/demo/Main.java
@@ -2,4 +2,12 @@ package org.cap.demo;
 
 public class Main {
 
+	public static void main(String[] args){
+		greetings();
+	}
+
+	private static void greetings() {
+		System.out.println("HI All");
+		
+	}
 }
